16394191e3ba636f9fb4b574f0a2812ea400686e
sharpiro <dsharpbb09@gmail.com>
2016-07-14 14:48:35 -0400

    Add project files.

---
+++ src/Monsters.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wells Fargo N.A.")]
+[assembly: AssemblyProduct("Monsters.Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("848df213-cfee-408b-8690-f9378eed4fda")]
